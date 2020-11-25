de6e24448d9f23da4994eb2bfe84c5e3b290dbac
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-11-25 19:59:47 +0300

    Rest api simple list action

---
+++ EmpManagementBackend/src/main/java/com/dursuneryilmaz/employeemanagement/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.dursuneryilmaz.employeemanagement.controller;
+
+import com.dursuneryilmaz.employeemanagement.domain.Employee;
+import com.dursuneryilmaz.employeemanagement.service.IEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private IEmployeeService employeeService;
+
+    //get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAll(){
+        return employeeService.getAll();
+    }
+}
---
+++ EmpManagementBackend/src/main/java/com/dursuneryilmaz/employeemanagement/service/IEmployeeService.java
@@ -0,0 +1,16 @@
+package com.dursuneryilmaz.employeemanagement.service;
+
+import com.dursuneryilmaz.employeemanagement.domain.Employee;
+
+import java.util.List;
+
+public interface IEmployeeService {
+    Employee add(Employee employee);
+
+    List<Employee> getAll();
+
+    Employee update(Employee employee);
+
+    void delete(Employee employee);
+
+}
