9f7613d4366ba31fa22dccbe3dba819d023c50f1
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-11-26 21:35:43 +0300

    first routing

--- emp-management-frontend/src/App.js
+++ emp-management-frontend/src/App.js
@@ -2,15 +2,22 @@ import "./App.css";
 import FooterComponent from "./components/FooterComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <HeaderComponent />
-      <div className="container">
-        <ListEmployeeComponent />
-      </div>
-      <FooterComponent />
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Switch>
+            <Route path="/" component={ListEmployeeComponent}></Route>
+            <Route path="/employees" component={ListEmployeeComponent}></Route>
+            <ListEmployeeComponent />
+          </Switch>
+        </div>
+        <FooterComponent />
+      </Router>
     </div>
   );
 }
